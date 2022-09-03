b1882b9efdcf7b9c13923238429ef2695b7cb918
hssw <hssw@gmx.de>
2022-09-03 15:32:22 +0200

    Added getUser

---
+++ getUser.py
@@ -0,0 +1,3 @@
+nutzerName = input("Bitte gib deinen Namen ein: ")
+nutzerAlter = input("Bitte gib dein Alter ein: ")
+print("Dein Name lautet " + nutzerName + " und du bist " + nutzerAlter + " Jahre alt.")
