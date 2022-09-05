6aa4fda54f6146f19fd274a6dac2c1c939a59c3a
hssw <hssw@gmx.de>
2022-09-05 10:12:12 +0200

    Fenster - extended 1

--- Fenster.py
+++ Fenster.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 # Die folgende Funktion soll ausgeführt werden, wenn
-# der Benutzer den Button Klick me anklickt
+# der Benutzer den Button "Klick mich" anklickt
 def button_action():
     entry_text = eingabefeld.get()
     if (entry_text == ""):
@@ -12,26 +12,30 @@ def button_action():
 
 fenster = Tk()
 fenster.title("Ich warte auf eine Eingabe von dir.")
+# Fenstergröße, Hintergrundfarbe
+fenster.geometry('600x300')
+fenster.configure(bg='lightgreen')
 
 # Anweisungs-Label
 my_label = Label(fenster, text="Gib deinen Namen ein: ")
 
 # In diesem Label wird nach dem Klick auf den Button der Benutzer
 # mit seinem eingegebenen Namen begrüsst.
-welcome_label = Label(fenster)
+# fg='#00ff00' (alternative Farbdefintion)
+welcome_label = Label(fenster, text="Moin ...", bg="yellow", fg="red")
 
 # Hier kann der Benutzer eine Eingabe machen
-eingabefeld = Entry(fenster, bd=2, width=80)
+eingabefeld = Entry(fenster, bd=2, width=50)
 
-welcom_button = Button(fenster, text="Klick mich", command=button_action)
+klickmich_button = Button(fenster, text="Klick mich", command=button_action)
 exit_button = Button(fenster, text="Beenden", command=fenster.quit)
 
 
 # Nun fügen wir die Komponenten unserem Fenster hinzu
-my_label.grid(row = 0, column = 0)
-eingabefeld.grid(row = 0, column = 1)
-welcom_button.grid(row = 1, column = 0)
-exit_button.grid(row = 1, column = 1)
-welcome_label.grid(row = 2, column = 0, columnspan = 2)
+my_label.grid(row = 0, column = 0, sticky="w")
+eingabefeld.grid(row = 1, column = 1, sticky="w")
+klickmich_button.grid(row = 2, column = 2, sticky="w")
+exit_button.grid(row = 3, column = 3, sticky="w")
+welcome_label.grid(row = 4, column = 1, columnspan = 1, sticky="w")
 
 mainloop()
